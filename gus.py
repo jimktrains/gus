2b65f45a857d219864b15f393bb2b62bb4f2e4be
Jim Keener <jim@jimkeener.com>
2013-01-09 16:41:52 -0500

    Separate Gus from anything dealing with the FS
    I wanted Gus to not be tied to any specific representation
    of the site (although it's original purpose was for directories)
    and I just felt it was cleaner if all the dirty filesystem touching
    code was cordoned off.
    
    Was it a good idea?
    Beats me.

--- gus.py
+++ gus.py
@@ -68,113 +68,18 @@ class Renderable:
 		return self.rendered
 
 
-class Gus:
-	def __init__(self, site_dir, dest_dir):
-		assert os.path.isdir(site_dir), "%s must be a dir" % site_dir
-		
-		properties_file = os.path.join(site_dir, "properties.yml")
-		assert os.path.isfile(properties_file), "%s must be a file" % properties_file
-		with open(properties_file, 'r') as f:
-			self.properties = yaml.load(f)
-		self.original_properties = copy.deepcopy(self.properties)
-		
-		pages_path      = os.path.join(site_dir, "pages")
-		assert os.path.isdir(pages_path), "%s must be a dir" % pages_path
-		self.pages_path = pages_path
-		
-		assets_path     = os.path.join(site_dir, "assets")
-		assert os.path.isdir(assets_path), "%s must be a dir" % assets_path
-		self.assets_path = assets_path
-		
-		rendered_path   = dest_dir
-		assert os.path.isdir(rendered_path), "%s must be a dir" % rendered_path
-		assert os.access(rendered_path, os.W_OK), "%s must be writable" % rendered_path
-		self.final_rendered_path = rendered_path
-		self.rendered_path = tempfile.mkdtemp()
-		
-		posts_path      = os.path.join(pages_path, "posts")
-		assert os.path.isdir(posts_path), "%s must be a dir" % posts_path
-		self.posts_path = posts_path
-		
-		top_level_path         = os.path.join(pages_path, "top-level")
-		assert os.path.isdir(top_level_path), "%s must be a dir" % top_level_path
-		self.top_level_path = top_level_path
-		
-		templates_path  = os.path.join(site_dir, "templates")
-		assert os.path.isdir(templates_path), "%s must be a dir" % templates_path
-		self.templates_path = templates_path
-
-		layout_template = os.path.join(templates_path, "layout.mustache")
-		assert os.path.isfile(layout_template), "%s must be a file" % layout_template
-		with open(layout_template, 'r') as f:
-			self.layout_template = f.read()
-		
-		posts_template  = os.path.join(templates_path, "post.mustache")
-		assert os.path.isfile(posts_template), "%s must be a file" % posts_template
-		with open(posts_template, 'r') as f:
-			self.posts_template = f.read()
-		self.posts_template = pystache.render(self.layout_template, { 'body': self.posts_template })
-		
-		top_level_template     = os.path.join(templates_path, "top-level.mustache")
-		assert os.path.isfile(top_level_template), "%s must be a file" % top_level_template
-		with open(top_level_template, 'r') as f:
-			self.top_level_template = f.read()
-		self.top_level_template = pystache.render(self.layout_template, { 'body': self.top_level_template })
+class Gus(object):
+	def __init__(self):
 		self.renderable = []
 		self.pages = {}
 		self.page_types = {
 			"posts": {
-				"template":   self.posts_template,
-				"path":       self.posts_path,
 				"final_path": 'posts'
 			},
 			"top-level": {
-				"template":   self.top_level_template,
-				"path":       self.top_level_path,
 				"final_path": ''
 			}
 		}
-		self.rendering = False
-
-	def render_pages(self):
-		for rend in self.renderable:
-			dest_file = os.path.join(self.rendered_path, rend.name + ".html")
-			dest_file_dir = os.path.dirname(dest_file)
-			if not os.path.isdir(dest_file_dir):
-				os.makedirs(dest_file_dir)
-			with open(dest_file, 'w+') as f:
-				f.write(rend.render());
-
-	# This method loads all of the pages into memory
-	# Then it performs some bookkeeping on them
-	def load_pages(self, path = None):
-		# This method loops over the filesystem and pulls
-		# all of the pages listed in page_types above.
-		# It then creates renderables for each
-		def handle_dir(page_type, info, dir_name, base_name=None):
-			if base_name is None:
-				base_name = dir_name
-			for dirname, dirnames, filenames in os.walk(dir_name):
-				for filename in filenames:
-					if filename == '.gitignore':
-						continue
-					if re.match("^\..*.sw.$", filename):
-						continue
-					if re.match(".*~$", filename):
-						continue
-					filename = os.path.join(dirname, filename)
-					rel_path = re.sub(base_name, '', filename)
-					rel_path = re.sub("^%s" % os.path.sep, '', rel_path)
-					rel_path = os.path.join(info['final_path'], rel_path)
-					name, markup_format = os.path.splitext(rel_path)
-					content = None
-					with open(filename, 'r') as f:
-						content = f.read()
-					print "renderable %s" % name
-					r = Renderable(name, info['template'], markup_format, content, self.properties, page_type)
-					self.renderable.append(r)
-		for page_type, info in self.page_types.items():
-			handle_dir(page_type, info, info["path"])
 
 	# This needs a better name
 	# basically it calculates all the lists of all the pages
@@ -199,6 +104,114 @@ class Gus:
 					'tag':      tag,
 					'pages': [ x.as_dict() for x in self.pages[page_type] if tag in x.metadata['tags'] ]
 				})
+
+	def get_web_path(self, page_type, name):
+		return os.path.sep + os.path.join(self.page_types[page_type]['final_path'], name)
+
+# This method goes through all of the steps to completly
+	# render a site, and then resets the object the make sure
+	# it's good to call again
+	def render_site(self):
+		self.load_pages()
+		self.calculate_properties()
+		self.render_pages()
+		self.copy_assets()
+		self.finalize()
+		# The rest of this basically resets the object
+		self.renderable = []
+		self.pages = {}
+		self.properties = copy.deepcopy(self.original_properties)
+		shutil.rmtree(self.rendered_path)
+		self.rendered_path = tempfile.mkdtemp()
+
+class GusFSAdapter(Gus):
+	def __init__(self, site_dir, dest_dir):
+		super(GusFSAdapter, self).__init__()
+		assert os.path.isdir(site_dir), "%s must be a dir" % site_dir
+		
+		properties_file = os.path.join(site_dir, "properties.yml")
+		assert os.path.isfile(properties_file), "%s must be a file" % properties_file
+		with open(properties_file, 'r') as f:
+			self.properties = yaml.load(f)
+		self.original_properties = copy.deepcopy(self.properties)
+		
+		pages_path      = os.path.join(site_dir, "pages")
+		assert os.path.isdir(pages_path), "%s must be a dir" % pages_path
+		self.pages_path = pages_path
+		
+		assets_path     = os.path.join(site_dir, "assets")
+		assert os.path.isdir(assets_path), "%s must be a dir" % assets_path
+		self.assets_path = assets_path
+		
+		rendered_path   = dest_dir
+		assert os.path.isdir(rendered_path), "%s must be a dir" % rendered_path
+		assert os.access(rendered_path, os.W_OK), "%s must be writable" % rendered_path
+		self.final_rendered_path = rendered_path
+		self.rendered_path = tempfile.mkdtemp()
+		
+		self.path = {}
+		posts_path      = os.path.join(pages_path, "posts")
+		assert os.path.isdir(posts_path), "%s must be a dir" % posts_path
+		self.path['posts'] = posts_path
+		
+		top_level_path         = os.path.join(pages_path, "top-level")
+		assert os.path.isdir(top_level_path), "%s must be a dir" % top_level_path
+		self.path['top-level'] = top_level_path
+		
+		templates_path  = os.path.join(site_dir, "templates")
+		assert os.path.isdir(templates_path), "%s must be a dir" % templates_path
+		self.templates_path = templates_path
+
+		self.templates = {}
+		layout_template = os.path.join(templates_path, "layout.mustache")
+		assert os.path.isfile(layout_template), "%s must be a file" % layout_template
+		with open(layout_template, 'r') as f:
+			self.templates['layout'] = f.read()
+		
+		posts_template  = os.path.join(templates_path, "post.mustache")
+		assert os.path.isfile(posts_template), "%s must be a file" % posts_template
+		with open(posts_template, 'r') as f:
+			self.templates['posts'] = f.read()
+		self.templates['posts'] = pystache.render(self.templates['layout'], { 'body': self.templates['posts'] })
+		
+		top_level_template     = os.path.join(templates_path, "top-level.mustache")
+		assert os.path.isfile(top_level_template), "%s must be a file" % top_level_template
+		with open(top_level_template, 'r') as f:
+			self.templates['top-level'] = f.read()
+		self.templates['top-level'] = pystache.render(self.templates['layout'], { 'body': self.templates['top-level'] })
+
+	def render_pages(self):
+		for rend in self.renderable:
+			dest_file = os.path.join(self.rendered_path, rend.name[1:] + ".html")
+			dest_file_dir = os.path.dirname(dest_file)
+			if not os.path.isdir(dest_file_dir):
+				os.makedirs(dest_file_dir)
+			with open(dest_file, 'w+') as f:
+				f.write(rend.render());
+
+	# This method loads all of the pages into memory
+	def load_pages(self):
+		for page_type, info in self.page_types.items():
+			base_name = self.path[page_type]
+			for dirname, dirnames, filenames in os.walk(base_name):
+				for filename in filenames:
+					if filename == '.gitignore':
+						continue
+					if re.match("^\..*.sw.$", filename):
+						continue
+					if re.match(".*~$", filename):
+						continue
+					filename = os.path.join(dirname, filename)
+					rel_path = re.sub(base_name, '', filename)
+					rel_path = re.sub("^%s" % os.path.sep, '', rel_path)
+					name, markup_format = os.path.splitext(rel_path)
+					name = self.get_web_path(page_type, name)
+					content = None
+					with open(filename, 'r') as f:
+						content = f.read()
+					print "renderable %s" % name
+					r = Renderable(name, self.templates[page_type], markup_format, content, self.properties, page_type)
+					self.renderable.append(r)
 
 	# This is the eq of doing cp -r directory/* other-dir
 	# It copies everything in a directory, but not the directory
@@ -227,18 +240,3 @@ class Gus:
 	def finalize(self):
 		self.copytree_wo_root(self.rendered_path, self.final_rendered_path)
 
-	# This method goes through all of the steps to completly
-	# render a site, and then resets the object the make sure
-	# it's good to call again
-	def render_site(self):
-		self.load_pages()
-		self.calculate_properties()
-		self.render_pages()
-		self.copy_assets()
-		self.finalize()
-		# The rest of this basically resets the object
-		self.renderable = []
-		self.pages = {}
-		self.properties = copy.deepcopy(self.original_properties)
-		shutil.rmtree(self.rendered_path)
-		self.rendered_path = tempfile.mkdtemp()
